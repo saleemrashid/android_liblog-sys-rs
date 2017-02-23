07a97406983fb632e457c4281199707466d121b1
Saleem Rashid <dev@saleemrashid.com>
2017-02-23 18:47:51 +0000

    Update template

--- src/lib.rs
+++ src/lib.rs
@@ -17,30 +17,25 @@ pub enum LogPriority {
 
 #[link(name = "log")]
 extern "C" {
-    pub fn __android_log_write(prio: c_int,
-                               tag: *const c_char,
-                               text: *const c_char) -> c_int;
+    pub fn __android_log_write(prio: c_int, tag: *const c_char, text: *const c_char) -> c_int;
 
-    pub fn __android_log_print(prio: c_int,
-                               tag: *const c_char,
-                               ...) -> c_int;
+    pub fn __android_log_print(prio: c_int, tag: *const c_char, ...) -> c_int;
 
     // pub fn __android_log_vprint(prio: c_int,
     //                            tag: *const c_char,
     //                            fmt: *const c_char,
     //                            ap: va_list) -> c_int;
 
-    pub fn __android_log_assert(prio: c_int,
-                                tag: *const c_char,
-                                fmt: *const c_char,
-                                ...) -> !;
+    pub fn __android_log_assert(prio: c_int, tag: *const c_char, fmt: *const c_char, ...) -> !;
 
     pub fn __android_log_is_loggable(prio: c_int,
                                      tag: *const c_char,
-                                     default_prio: c_int) -> c_int;
+                                     default_prio: c_int)
+                                     -> c_int;
 
     pub fn __android_log_is_loggable_len(prio: c_int,
                                          tag: *const c_char,
                                          len: size_t,
-                                         default_prio: c_int) -> c_int;
+                                         default_prio: c_int)
+                                         -> c_int;
 }
